faa166731eac18a517353cc2d006c3d5b744118a
emelanson <elijahmelanson@gmail.com>
2020-11-21 21:18:22 -0500

    Loaded and parsed database json

--- Develop/server.js
+++ Develop/server.js
@@ -8,6 +8,14 @@ var PORT = 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+fs.readFile(path.join(__dirname, 'db/db.json'), (err, data) => {
+    if (err) throw err;
+    let database = JSON.parse(data);
+    console.log(database);
+});
+
+
 app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"), (err, data) => {
         if (err) throw err;
